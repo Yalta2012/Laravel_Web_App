7756ec17f7b86d59000fbd5d8d5911c48191cd48
egor <egor>
2026-03-17 01:40:30 +0500

    sanctum installed

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Laravel\Sanctum\HasApiTokens;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $user = User::where('email', $fields['email'])->first();
+
+        if(!$user){
+            return response([
+                "message" => 'Wrong email'
+                ], 401);
+        }
+
+        if(!Hash::check($fields['password'], $user->password)){
+            return response([
+                'message' => 'password'
+                ], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request){
+        auth()->user()->tokens()->delete();
+        return response([
+            'message' => "Logged off"
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,16 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AddressController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\PropertyTypeController;
 use App\Http\Controllers\UserController;
+// use Laravel\Sanctum\Middleware;
+
+
+Route::post('/login', [AuthController::class, 'login']);
 
 
 
@@ -17,5 +23,13 @@ Route::get('/property_type/{id}', [PropertyTypeController::class, 'show']);
 Route::get('/property', [PropertyController::class, 'index']); 
 Route::get('/property/{id}', [PropertyController::class, 'show']); 
 
-Route::get('/user', [UserController::class, 'index']); 
-Route::get('/user/{id}', [UserController::class, 'show']); 
+// Route::get('/user', [UserController::class, 'index']); 
+// Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'index']); 
+
+Route::group(['middleware' => ['auth:sanctum']],
+            function(){
+                Route::get('/user', function(Request $request){return $request->user();});
+                Route::get('/logout', [AuthController::class, 'logout']); 
+                Route::get('/user/{id}', [UserController::class, 'show']);
+            }
+);
